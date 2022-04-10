d95f9147d1ae39b60ba418e7cc62755ce72b4196
priyabrata-cpp <76444246+priyabrata-cpp@users.noreply.github.com>
2022-04-10 13:02:07 +0530

    Add files via upload

---
+++ precache-manifest.523f4c63253d44f33eb34a92840be12a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "241b76e92ab353786a14",
+    "url": "./static/css/main.f1da7198.chunk.css"
+  },
+  {
+    "revision": "241b76e92ab353786a14",
+    "url": "./static/js/main.241b76e9.chunk.js"
+  },
+  {
+    "revision": "dc2c6ecaabfff3523052",
+    "url": "./static/js/1.dc2c6eca.chunk.js"
+  },
+  {
+    "revision": "b1be4021c83f951bbba0",
+    "url": "./static/js/2.b1be4021.chunk.js"
+  },
+  {
+    "revision": "3b23b0e64e78688101da",
+    "url": "./static/js/runtime~main.3b23b0e6.js"
+  },
+  {
+    "revision": "96737f1c3fc2cabf06864118b1f7f54f",
+    "url": "./static/media/pn.96737f1c.jpg"
+  },
+  {
+    "revision": "361b2cfc6b21133bf3423ad56f60c3c1",
+    "url": "./static/media/hero.361b2cfc.jpg"
+  },
+  {
+    "revision": "1ec1930aa0758728c9f196f64fd7d3bb",
+    "url": "./static/media/Priya.1ec1930a.jpg"
+  },
+  {
+    "revision": "32ceb179585b9fcaba434ca36afd32c4",
+    "url": "./index.html"
+  }
+];
